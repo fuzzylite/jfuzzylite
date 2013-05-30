58c8ed010f74d5d569dca58632a3989cb9082b74
jcrada <jcrada@gmail.com>
2013-05-31 08:20:49 +1200

    jFuzzylite officially started

---
+++ jfuzzylite/src/com/fuzzylite/terms/Term.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fuzzylite.terms;
+
+/**
+ *
+ * @author jcrada
+ */
+public class Term {
+
+    public Term() {
+    }
+}
