897d5b0032d9c33ae2621ab25142cd8e9889bb1f
jcrada <jcrada@gmail.com>
2013-11-04 22:33:38 +1300

    Function: Operator and BuiltInFunction.

--- jfuzzylite/src/com/fuzzylite/term/Function.java
+++ jfuzzylite/src/com/fuzzylite/term/Function.java
@@ -18,10 +18,11 @@ import com.fuzzylite.Engine;
 import com.fuzzylite.FuzzyLite;
 import com.fuzzylite.Op;
 import static com.fuzzylite.Op.str;
+import com.fuzzylite.rule.Rule;
 import com.fuzzylite.variable.InputVariable;
 import com.fuzzylite.variable.OutputVariable;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -33,21 +34,54 @@ public class Function extends Term {
     public static abstract class Element {
 
         public String name;
-        public Method function;
+        public Method method;
 
         public int getArity() {
-            return function.getParameterTypes().length;
+            return method.getParameterTypes().length;
         }
     }
 
     public static class Operator extends Element {
 
-        public short precedence, associativity;
+        public int precedence, associativity;
+
+        public Operator() {
+            this("", null, 0);
+        }
+
+        public Operator(String name, Method operator, int precedence) {
+            this(name, operator, precedence, -1);
+        }
+
+        public Operator(String name, Method operator, int precedence, int associativity) {
+            this.name = name;
+            this.method = operator;
+            this.precedence = precedence;
+            this.associativity = associativity;
+        }
     }
 
     public static class BuiltInFunction extends Element {
 
-        public short associativity;
+        public int associativity;
+
+        public BuiltInFunction() {
+            this("", null);
+        }
+
+        public BuiltInFunction(Method function) {
+            this(function.getName(), function);
+        }
+
+        public BuiltInFunction(String name, Method function) {
+            this(name, function, -1);
+        }
+
+        public BuiltInFunction(String name, Method function, int associativity) {
+            this.name = name;
+            this.method = function;
+            this.associativity = associativity;
+        }
     }
 
     public static class Node {
@@ -69,14 +103,14 @@ public class Function extends Term {
                 try {
                     switch (element.getArity()) {
                         case 0:
-                            result = (double) element.function.invoke(null);
+                            result = (double) element.method.invoke(null);
                             break;
                         case 1:
-                            result = (double) element.function.invoke(null,
+                            result = (double) element.method.invoke(null,
                                     left.evaluate(localVariables));
                             break;
                         case 2:
-                            result = (double) element.function.invoke(null,
+                            result = (double) element.method.invoke(null,
                                     left.evaluate(localVariables),
                                     right.evaluate(localVariables));
                             break;
@@ -106,11 +140,6 @@ public class Function extends Term {
             }
             FuzzyLite.logger().info(String.format("%s=%s", toPostfix(), str(result)));
             return result;
-        }
-
-        public static Node parse(String infix) throws Exception {
-            //TODO: implement
-            return null;
         }
 
         @Override
@@ -207,19 +236,23 @@ public class Function extends Term {
     protected Map<String, Operator> operators;
     protected Map<String, BuiltInFunction> functions;
 
-    public Function(String name) {
-
+    public Function() {
+        this("");
     }
 
-    public Function(String name, String text, Engine engine, boolean loadBuiltInFunctions) {
+    public Function(String name) {
+        this(name, "", null);
+    }
+
+    public Function(String name, String text, Engine engine) {
         this.name = name;
         this.text = text;
         this.engine = engine;
-        //TODO: What to do with warning? Overridable method.
-        loadOperators();
-        if (loadBuiltInFunctions) {
-            loadBuiltInFunctions();
-        }
+        this.root = null;
+        this.variables = new HashMap<>();
+        this.operators = new HashMap<>();
+        this.functions = new HashMap<>();
+        this.loadOperators();
     }
 
     public void load() throws Exception {
@@ -258,25 +291,100 @@ public class Function extends Term {
 
     @Override
     public String toString() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return String.format("Function (%s)", this.text);
     }
 
-    public static Function create(String name, String text, Engine engine) {
-        //TODO: implement
+    public static Function create(String name, String text, Engine engine, boolean requiresFunctions) throws Exception {
         Function result = new Function(name);
+        if (requiresFunctions) {
+            result.loadBuiltInFunctions();
+        }
+        result.load(text, engine);
         return result;
     }
 
-    public void loadOperators() {
-        //TODO: implement
+    private void loadOperators() {
+        int p = 7;
+        try {
+            // (!) Logical and (~) Bitwise NOT
+            //        this->_unaryOperators["!"] = new Operator("!", std::logical_not<scalar>, p, 1);
+            // ~ negates a number
+            this.operators.put("~", new Operator("~",
+                    Op.class.getMethod("negate", double.class), p, 1));
+            --p; //Power
+            this.operators.put("^", new Operator("^",
+                    Math.class.getMethod("pow", double.class, double.class), p, 1));
+            --p; //Multiplication, Division, and Modulo
+            this.operators.put("*", new Operator("*",
+                    Op.class.getMethod("multiply", double.class, double.class), p));
+            this.operators.put("/", new Operator("/",
+                    Op.class.getMethod("divide", double.class, double.class), p));
+            this.operators.put("%", new Operator("%",
+                    Op.class.getMethod("modulo", double.class, double.class), p));
+            --p; //Addition, Subtraction
+            this.operators.put("+", new Operator("+",
+                    Op.class.getMethod("add", double.class, double.class), p));
+            this.operators.put("-", new Operator("-",
+                    Op.class.getMethod("subtract", double.class, double.class), p));
+//        //        --p; //Bitwise AND
+//        //        this->_binaryOperators["&"] = new Operator("&", std::bit_and, p);
+//        //        --p; //Bitwise OR
+//        //        this->_binaryOperators["|"] = new Operator("|", std::bit_or, p);
+            --p; //Logical AND
+            this.operators.put(Rule.FL_AND, new Operator(Rule.FL_AND,
+                    Op.class.getMethod("logicalAnd", double.class, double.class), p));
+            --p; //Logical OR
+            this.operators.put(Rule.FL_OR, new Operator(Rule.FL_OR,
+                    Op.class.getMethod("logicalOr", double.class, double.class), p));
+        } catch (NoSuchMethodException | SecurityException ex) {
+            throw new RuntimeException("[function error] operator could not be loaded", ex);
+        }
     }
 
     public void loadBuiltInFunctions() {
-        //TODO: implement
+        try {
+            this.functions.put("acos", new BuiltInFunction(Math.class.getMethod("acos", double.class)));
+            this.functions.put("asin", new BuiltInFunction(Math.class.getMethod("asin", double.class)));
+            this.functions.put("atan", new BuiltInFunction(Math.class.getMethod("atan", double.class)));
+
+            this.functions.put("ceil", new BuiltInFunction(Math.class.getMethod("ceil", double.class)));
+            this.functions.put("cos", new BuiltInFunction(Math.class.getMethod("cos", double.class)));
+            this.functions.put("cosh", new BuiltInFunction(Math.class.getMethod("cosh", double.class)));
+            this.functions.put("exp", new BuiltInFunction(Math.class.getMethod("exp", double.class)));
+            this.functions.put("fabs", new BuiltInFunction(Math.class.getMethod("abs", double.class)));
+            this.functions.put("floor", new BuiltInFunction(Math.class.getMethod("floor", double.class)));
+            this.functions.put("log", new BuiltInFunction(Math.class.getMethod("log", double.class)));
+            this.functions.put("log10", new BuiltInFunction(Math.class.getMethod("log10", double.class)));
+
+            this.functions.put("sin", new BuiltInFunction(Math.class.getMethod("sin", double.class)));
+            this.functions.put("sinh", new BuiltInFunction(Math.class.getMethod("sinh", double.class)));
+            this.functions.put("sqrt", new BuiltInFunction(Math.class.getMethod("sqrt", double.class)));
+            this.functions.put("tan", new BuiltInFunction(Math.class.getMethod("tan", double.class)));
+            this.functions.put("tanh", new BuiltInFunction(Math.class.getMethod("tanh", double.class)));
+
+            this.functions.put("log1p", new BuiltInFunction(Math.class.getMethod("log1p", double.class)));
+            //not found in Java
+//            this.functions.put("acosh", new BuiltInFunction("acosh",  & (acosh)));
+//            this.functions.put("asinh", new BuiltInFunction("asinh",  & (asinh)));
+//            this.functions.put("atanh", new BuiltInFunction("atanh",  & (atanh)));
+
+            this.functions.put("pow", new BuiltInFunction(Math.class.getMethod("pow", double.class, double.class)));
+            this.functions.put("atan2", new BuiltInFunction(Math.class.getMethod("atan2", double.class, double.class)));
+            this.functions.put("fmod", new BuiltInFunction(Op.class.getMethod("modulo", double.class, double.class)));
+        } catch (Exception ex) {
+            throw new RuntimeException("[function error] built-in functions could not be loaded", ex);
+        }
     }
 
     public String toPostfix(String text) {
-        //TODO: implement
+        return toPostfix(text, false);
+    }
+
+    public String toPostfix(String text, boolean requiresFunctions) {
+        //TODO:implement
+        if (requiresFunctions) {
+            loadBuiltInFunctions();
+        }
         return "";
     }
 
@@ -285,13 +393,42 @@ public class Function extends Term {
         return null;
     }
 
-    public static void main(String[] args) throws Exception {
-        System.out.println(Op.class.getMethod("add", double.class, double.class).getParameterTypes().length);
-        for (Method m : Math.class.getMethods()) {
+    public String getText() {
+        return text;
+    }
 
-            if (Modifier.isPublic(m.getModifiers())
-                    && Modifier.isStatic(m.getModifiers())) {
-            }
-        }
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public Map<String, Double> getVariables() {
+        return variables;
+    }
+
+    public Map<String, Operator> getOperators() {
+        return operators;
+    }
+
+    public Map<String, BuiltInFunction> getFunctions() {
+        return functions;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Function f = new Function();
+        f.loadOperators();
+        f.loadBuiltInFunctions();
+        System.out.println(f.functions.get("pow").method.invoke(null, 3, 3));
     }
 }
