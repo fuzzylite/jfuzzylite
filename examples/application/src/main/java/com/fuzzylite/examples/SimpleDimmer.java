3373352f68383938efcae5370983b224f0d7644f
Juan Rada-Vilela <jcrada@fuzzylite.com>
2015-04-17 17:52:44 +1200

    Example application updated

--- examples/application/src/main/java/com/fuzzylite/examples/SimpleDimmer.java
+++ examples/application/src/main/java/com/fuzzylite/examples/SimpleDimmer.java
@@ -26,28 +26,85 @@ package com.fuzzylite.examples;
 
 import com.fuzzylite.Engine;
 import com.fuzzylite.FuzzyLite;
+import com.fuzzylite.defuzzifier.Centroid;
 import com.fuzzylite.imex.FldExporter;
 import com.fuzzylite.imex.FllImporter;
+import com.fuzzylite.norm.s.Maximum;
+import com.fuzzylite.norm.t.Minimum;
+import com.fuzzylite.rule.Rule;
+import com.fuzzylite.rule.RuleBlock;
+import com.fuzzylite.term.Triangle;
+import com.fuzzylite.variable.InputVariable;
+import com.fuzzylite.variable.OutputVariable;
 import java.io.File;
 import java.net.URL;
+import java.util.logging.Level;
 
 public class SimpleDimmer {
 
-    private Engine engine;
-
     public SimpleDimmer() {
+
+    }
+
+    //Method 1: Set up the engine manually.
+    public void method1() {
+        Engine engine = new Engine();
+        engine.setName("simple-dimmer");
+
+        InputVariable inputVariable = new InputVariable();
+        inputVariable.setEnabled(true);
+        inputVariable.setName("Ambient");
+        inputVariable.setRange(0.000, 1.000);
+        inputVariable.addTerm(new Triangle("DARK", 0.000, 0.250, 0.500));
+        inputVariable.addTerm(new Triangle("MEDIUM", 0.250, 0.500, 0.750));
+        inputVariable.addTerm(new Triangle("BRIGHT", 0.500, 0.750, 1.000));
+        engine.addInputVariable(inputVariable);
+
+        OutputVariable outputVariable = new OutputVariable();
+        outputVariable.setEnabled(true);
+        outputVariable.setName("Power");
+        outputVariable.setRange(0.000, 1.000);
+        outputVariable.fuzzyOutput().setAccumulation(new Maximum());
+        outputVariable.setDefuzzifier(new Centroid(200));
+        outputVariable.setDefaultValue(Double.NaN);
+        outputVariable.setLockPreviousOutputValue(false);
+        outputVariable.setLockOutputValueInRange(false);
+        outputVariable.addTerm(new Triangle("LOW", 0.000, 0.250, 0.500));
+        outputVariable.addTerm(new Triangle("MEDIUM", 0.250, 0.500, 0.750));
+        outputVariable.addTerm(new Triangle("HIGH", 0.500, 0.750, 1.000));
+        engine.addOutputVariable(outputVariable);
+
+        RuleBlock ruleBlock = new RuleBlock();
+        ruleBlock.setEnabled(true);
+        ruleBlock.setName("");
+        ruleBlock.setConjunction(null);
+        ruleBlock.setDisjunction(null);
+        ruleBlock.setActivation(new Minimum());
+        ruleBlock.addRule(Rule.parse("if Ambient is DARK then Power is HIGH", engine));
+        ruleBlock.addRule(Rule.parse("if Ambient is MEDIUM then Power is MEDIUM", engine));
+        ruleBlock.addRule(Rule.parse("if Ambient is BRIGHT then Power is LOW", engine));
+        engine.addRuleBlock(ruleBlock);
+
+        FuzzyLite.logger().info(new FldExporter().toString(engine));
+    }
+    
+    //Method 2: Load from a file
+    public void method2(){
+        Engine engine = null;
         String configurationFile = "/SimpleDimmer.fll";
         URL url = SimpleDimmer.class.getResource(configurationFile);
         try {
             engine = new FllImporter().fromFile(new File(url.toURI()));
         } catch (Exception ex) {
-            FuzzyLite.logger().severe(ex.toString());
+            FuzzyLite.logger().log(Level.SEVERE, ex.toString(), ex);
         }
+        
+        FuzzyLite.logger().info(new FldExporter().toString(engine));
     }
-
-    public void run() {
-        FldExporter exporter = new FldExporter();
-        FuzzyLite.logger().info(exporter.toString(engine, 10 * engine.getInputVariables().size()));
+    
+    public void run(){
+        method1();
+        method2();
     }
 
     public static void main(String[] args) {
