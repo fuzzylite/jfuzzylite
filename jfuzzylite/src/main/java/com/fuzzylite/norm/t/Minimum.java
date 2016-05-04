7ea275f19774df55275f7493b5e7483db89923e5
Juan Rada-Vilela <jcrada@fuzzylite.com>
2016-05-04 18:40:58 +1200

    FuzzyLite License

--- jfuzzylite/src/main/java/com/fuzzylite/norm/t/Minimum.java
+++ jfuzzylite/src/main/java/com/fuzzylite/norm/t/Minimum.java
@@ -1,27 +1,20 @@
 /*
- Author: Juan Rada-Vilela, Ph.D.
- Copyright (C) 2010-2014 FuzzyLite Limited
- All rights reserved
+ Copyright © 2010-2016 by FuzzyLite Limited.
+ All rights reserved.
 
- This file is part of jfuzzylite.
+ This file is part of jfuzzylite™.
 
- jfuzzylite is free software: you can redistribute it and/or modify it under
- the terms of the GNU Lesser General Public License as published by the Free
- Software Foundation, either version 3 of the License, or (at your option)
- any later version.
+ jfuzzylite™ is free software: you can redistribute it and/or modify it under
+ the terms of the FuzzyLite License included with the software.
 
- jfuzzylite is distributed in the hope that it will be useful, but WITHOUT
- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
- for more details.
+ You should have received a copy of the FuzzyLite License along with 
+ jfuzzylite™. If not, see <http://www.fuzzylite.com/license/>.
 
- You should have received a copy of the GNU Lesser General Public License
- along with jfuzzylite.  If not, see <http://www.gnu.org/licenses/>.
-
- fuzzylite™ is a trademark of FuzzyLite Limited.
+ fuzzylite® is a registered trademark of FuzzyLite Limited.
  jfuzzylite™ is a trademark of FuzzyLite Limited.
 
  */
+
 package com.fuzzylite.norm.t;
 
 import com.fuzzylite.norm.TNorm;
