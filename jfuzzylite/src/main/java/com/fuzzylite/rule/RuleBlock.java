0d1be85f9a57f41a1d5d58a8233bd7c4f6397fec
Juan Rada-Vilela <jcrada@fuzzylite.com>
2016-05-09 18:47:53 +1200

    RuleBlock is Cloneable

--- jfuzzylite/src/main/java/com/fuzzylite/rule/RuleBlock.java
+++ jfuzzylite/src/main/java/com/fuzzylite/rule/RuleBlock.java
@@ -29,7 +29,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
 
-public class RuleBlock {
+public class RuleBlock implements Op.Cloneable{
 
     private String name;
     private TNorm conjunction;
