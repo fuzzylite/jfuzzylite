b0555c45d7133df4537056d1f4f7fdf0e9190264
Juan Rada-Vilela <jcrada@fuzzylite.com>
2016-10-29 20:31:50 +1300

    FuzzyLite License

--- jfuzzylite/src/test/java/com/fuzzylite/factory/FunctionFactoryTest.java
+++ jfuzzylite/src/test/java/com/fuzzylite/factory/FunctionFactoryTest.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ jfuzzylite (TM), a fuzzy logic control library in Java.
+ Copyright (C) 2010-2016 FuzzyLite Limited. All rights reserved.
+ Author: Juan Rada-Vilela, Ph.D. <jcrada@fuzzylite.com>
+
+ This file is part of jfuzzylite.
+
+ jfuzzylite is free software: you can redistribute it and/or modify it under
+ the terms of the FuzzyLite License included with the software.
+
+ You should have received a copy of the FuzzyLite License along with
+ jfuzzylite. If not, see <http://www.fuzzylite.com/license/>.
+
+ jfuzzylite is a trademark of FuzzyLite Limited.
+ fuzzylite (R) is a registered trademark of FuzzyLite Limited.
  */
 package com.fuzzylite.factory;
 
@@ -19,10 +30,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
 
-/**
- *
- * @author juan
- */
 public class FunctionFactoryTest {
 
     public FunctionFactoryTest() {
