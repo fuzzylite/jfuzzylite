cbe328368e87335a2d2eaf38bd79c6a4a1898e94
Juan Rada-Vilela <jcrada@fuzzylite.com>
2017-03-21 04:39:53 +1300

    Quick test

---
+++ jfuzzylite/src/test/java/QuickTest.java
@@ -0,0 +1,54 @@
+/*
+ jfuzzylite (TM), a fuzzy logic control library in Java.
+ Copyright (C) 2010-2017 FuzzyLite Limited. All rights reserved.
+ Author: Juan Rada-Vilela, Ph.D. <jcrada@fuzzylite.com>
+
+ This file is part of jfuzzylite.
+
+ jfuzzylite is free software: you can redistribute it and/or modify it under
+ the terms of the FuzzyLite License included with the software.
+
+ You should have received a copy of the FuzzyLite License along with
+ jfuzzylite. If not, see <http://www.fuzzylite.com/license/>.
+
+ jfuzzylite is a trademark of FuzzyLite Limited.
+ fuzzylite (R) is a registered trademark of FuzzyLite Limited.
+ */
+import com.fuzzylite.term.Function;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author juan
+ */
+public class QuickTest {
+
+    public QuickTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testParsingDoubleParenthesis() {
+        new Function().toPostfix("if ((Ambient is DARK)) then Power is HIGH");
+    }
+
+}
